064c490d62fdfac030d2de5fc0460cc27c75307c
Kwasi <kwasi.owusumari@verisure.com>
2026-03-03 17:44:08 +0100

    Can retrieve weather from API

---
+++ backend/DashyBoard.Application/Queries/Weather/GetWeatherSymbolQuery.cs
@@ -0,0 +1,7 @@
+﻿using DashyBoard.Application.Queries.Weather.Dto;
+using MediatR;
+
+namespace DashyBoard.Application.Queries.Weather
+{
+    public sealed record GetWeatherSymbolQuery(string longi, string lati) : IRequest<WeatherSymbolDto>;
+}
---
+++ backend/DashyBoard.Application/Queries/Weather/GetWeatherSymbolQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using DashyBoard.Application.Interfaces;
+using DashyBoard.Application.Queries.Weather.Dto;
+using MediatR;
+
+namespace DashyBoard.Application.Queries.Weather
+{
+    public sealed class GetWeatherSymbolQueryHandler : IRequestHandler<GetWeatherSymbolQuery, WeatherSymbolDto>
+    {
+        private readonly IWeatherApiClient _weatherClient;
+
+        public GetWeatherSymbolQueryHandler(IWeatherApiClient weatherClient)
+        {
+            _weatherClient = weatherClient;
+        }
+
+        public async Task<WeatherSymbolDto> Handle(GetWeatherSymbolQuery request, CancellationToken cancellationToken)
+        {
+            return await _weatherClient.GetWeatherSymbolAsync(request.longi, request.lati, cancellationToken);
+        }
+    }
+}
