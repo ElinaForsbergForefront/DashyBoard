70bc1f0eb7178c3ac177f5a78bfb738e5f633fa4
Simon Sjoo <simon.sjoo@forefront.se>
2026-03-09 10:49:36 +0100

    Gold endpoints

--- frontend/src/api/apiSlice.ts
+++ frontend/src/api/apiSlice.ts
@@ -4,6 +4,6 @@ import { baseQuery } from './BaseQuery';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery,
-  tagTypes: ['User'],
+  tagTypes: ['User', 'Gold'],
   endpoints: () => ({}),
 });
