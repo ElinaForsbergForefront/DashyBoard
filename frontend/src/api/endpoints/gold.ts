98081cfb8d2186cfc47f06489a2852f1f44f1bbd
Simon Sjöö <simon.sjoo@forefront.se>
2026-03-12 15:11:42 +0100

    Ändrade till små bokstäver

--- frontend/src/api/endpoints/gold.ts
+++ frontend/src/api/endpoints/gold.ts
@@ -4,11 +4,11 @@ import type { AssetPriceDto, AssetTickersDto } from '../types/gold';
 const goldApi = api.injectEndpoints({
   endpoints: (builder) => ({
     getGoldTickers: builder.query<AssetTickersDto[], void>({
-      query: () => '/Gold/tickers',
+      query: () => '/gold/tickers',
       providesTags: [{ type: 'Gold', id: 'TICKERS' }],
     }),
     getGoldPrice: builder.query<AssetPriceDto, string>({
-      query: (symbol) => `/Gold/price/${symbol}`,
+      query: (symbol) => `/gold/price/${symbol}`,
       providesTags: (_result, _error, symbol) => [{ type: 'Gold', id: symbol }],
     }),
   }),
