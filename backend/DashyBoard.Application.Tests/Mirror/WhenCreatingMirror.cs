cfde9b5c2d3c24ec22149291e1eced6a6699aa2c
Simon Sjöö <simon.sjoo@forefront.se>
2026-03-12 14:24:18 +0100

    Tests för mirror

---
+++ backend/DashyBoard.Application.Tests/Mirror/WhenCreatingMirror.cs
@@ -0,0 +1,35 @@
+﻿using DashyBoard.Application.Commands.Mirror;
+using DashyBoard.Application.Interfaces;
+using DashyBoard.Application.Queries.Mirror.Dto;
+using Moq;
+
+namespace DashyBoard.Application.Tests.Mirror;
+
+public class WhenCreatingMirror
+{
+    [Test]
+    public async Task ThenShouldCreateMirror()
+    {
+        var expectedMirror = new MirrorDto
+        {
+            Id = Guid.NewGuid(),
+            UserId = "auth0|123",
+            Name = "My Mirror",
+            WidthCm = 100,
+            HeightCm = 50,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        var mock = new Mock<IMirrorRepository>();
+        mock
+            .Setup(x => x.CreateMirrorAsync("auth0|123", "My Mirror", 100, 50, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedMirror);
+
+        var handler = new CreateMirrorCommandHandler(mock.Object);
+        var command = new CreateMirrorCommand("auth0|123", "My Mirror", 100, 50);
+
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        mock.Verify(x => x.CreateMirrorAsync("auth0|123", "My Mirror", 100, 50, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ backend/DashyBoard.Application.Tests/Mirror/WhenUpdatingMirror.cs
@@ -0,0 +1,28 @@
+﻿using DashyBoard.Application.Commands.Mirror;
+using DashyBoard.Application.Interfaces;
+using DashyBoard.Application.Queries.Mirror.Dto;
+using Moq;
+
+namespace DashyBoard.Application.Tests.Mirror;
+
+public class WhenUpdatingMirror
+{
+    [Test]
+    public async Task ThenShouldUpdateMirror()
+    {
+        var mirrorId = Guid.NewGuid();
+        var expectedMirror = new MirrorDto { Id = mirrorId, UserId = "auth0|123", Name = "Updated", WidthCm = 200, HeightCm = 75 };
+
+        var mock = new Mock<IMirrorRepository>();
+        mock
+            .Setup(x => x.UpdateMirrorAsync(mirrorId, "Updated", 200, 75, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedMirror);
+
+        var handler = new UpdateMirrorCommandHandler(mock.Object);
+        var command = new UpdateMirrorCommand(mirrorId, "Updated", 200, 75);
+
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        mock.Verify(x => x.UpdateMirrorAsync(mirrorId, "Updated", 200, 75, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
