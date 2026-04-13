aefcadead666662eab5ce0582f7b3c40ef331a8f
Elina Forsberg <elina.forsberg@sveaskog.se>
2026-04-13 14:55:01 +0200

    lint

--- frontend/router.tsx
+++ frontend/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from './src/App';
 import { StyleGuide } from './src/pages/StyleGuide';
 import { Mirrors } from './src/pages/Mirrors';
-import { Widgets } from './src/pages/widgets';
+import { Widgets } from './src/pages/Widgets';
 
 export const router = createBrowserRouter([
   {
