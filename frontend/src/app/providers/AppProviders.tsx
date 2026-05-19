8a6eb7108ab521e339c1c08fe968fbbd554aff08
Simon Sjöö <simon.sjoo@forefront.se>
2026-05-19 10:21:48 +0200

    Add SignalR integration for real-time friend notifications and refactor token handling

--- frontend/src/app/providers/AppProviders.tsx
+++ frontend/src/app/providers/AppProviders.tsx
@@ -4,15 +4,19 @@ import { store } from '../store';
 import { AuthTokenInjector } from '../../api/AuthTokenInjector';
 import { ThemeProvider } from '../../context/ThemeContext';
 import { ActiveMirrorProvider } from '../../context/ActiveMirrorContext';
+import { ToastProvider } from './ToastProvider';
+import { FriendRealtimeProvider } from './FriendRealtimeProvider';
 
 export function AppProviders({ children }: PropsWithChildren) {
   return (
     <ThemeProvider>
       <Provider store={store}>
-        <AuthTokenInjector />
-        <ActiveMirrorProvider>
-          {children}
-        </ActiveMirrorProvider>
+        <ToastProvider>
+          <AuthTokenInjector />
+          <FriendRealtimeProvider>
+            <ActiveMirrorProvider>{children}</ActiveMirrorProvider>
+          </FriendRealtimeProvider>
+        </ToastProvider>
       </Provider>
     </ThemeProvider>
   );
